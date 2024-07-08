d0adc9c91c45fc1ca1ec05c186163ccc0d932480
Ana Ramirez <00183224@uca.edu.sv>
2024-07-08 14:38:45 -0600

    Agregando dentro de rama main, switch y do while principal

--- PRUEBAFINAL.cpp
+++ PRUEBAFINAL.cpp
@@ -8,6 +8,46 @@ using namespace std;
 
 /*FUNCION MAIN*/
 int main(void)
-{
+{   
+    /*VARIABLES DECLARADAS*/
+    int opc; /*VARIABLE PARA SWITCH PRINCIPAL*/
+
+    /*DO WHILE PARA SWITCH PRINCIPAL*/
+    do {
+
+        /*SWITCH PRINCIPAL*/
+        switch (opc) {
+            case 1: 
+
+                break;
+            
+            case 2: 
+
+                break; 
+            
+            case 3: 
+
+                break;
+            
+            case 4:
+
+                break;
+            
+            case 5: 
+
+                break;
+
+            case 6:
+
+                cout << "SALIR " << endl;
+
+                break;
+
+            default: 
+            
+                break;
+        }
+    } while (opc != 6);
+
     return 0;
 }
