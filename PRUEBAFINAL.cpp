aa03577d9141095e2477024911b18b914ef69b95
Ana Ramirez <00183224@uca.edu.sv>
2024-07-08 14:36:53 -0600

    Anadiendo librerias y funcion main

---
+++ PRUEBAFINAL.cpp
@@ -0,0 +1,13 @@
+/*LIBRERIAS*/
+#include "iostream"
+#include "string"
+#include "ctime"
+#include "cstdlib"
+#include "unistd.h"
+using namespace std;
+
+/*FUNCION MAIN*/
+int main(void)
+{
+    return 0;
+}
