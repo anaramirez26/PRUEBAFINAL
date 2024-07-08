12135a5586ab5cc4aac0b35a9fe411c523ae3d15
Ana Ramirez <00183224@uca.edu.sv>
2024-07-08 15:12:24 -0600

    Agregando funcion para que cuando el bot diga un numero que coincida con alguno de los tableros se marquen los numeros en el tablero reemplazandolos por cero

--- PRUEBAFINAL.cpp
+++ PRUEBAFINAL.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 /*PROTOTIPO DE FUNCION*/
 bool yaColocado(int, int mat[3][3]);
+void marcarNumero(int, int mat[3][3]);
 
 /*FUNCION MAIN*/
 int main(void)
@@ -92,11 +93,16 @@ int main(void)
                     numAl = 1 + rand() % 20;
                     cout << endl << endl << "Numero generado por el bot: " << numAl << endl << endl;
 
+                    /*LLAMA A LA FUNCION MARCAR NUMERO*/
+                    for (int k = 0; k < 2; k++) {
+                        marcarNumero(numAl, mat[k]);
+                    }
+
                     /*PAUSA ENTRE GENERACIONES DE NUMEROS*/
                     sleep(1);
 
                 }
-                
+
                 break;
             }
             
@@ -150,4 +156,16 @@ bool yaColocado(int num, int mat[3][3])
         }
     }
     return false;
+}
+
+/*FUNCION PARA MARCAR UN NUMERO EN EL TABLERO REEMPLAZANDOLO POR CERO*/
+void marcarNumero(int num, int mat[3][3])
+{
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (mat[i][j] == num) {
+                mat[i][j] = 0;
+            }
+        }
+    }
 }
