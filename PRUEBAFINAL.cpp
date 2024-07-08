46328ee572e1fae06504c95e5f6cdbcb8593c5d5
Ana Ramirez <00183224@uca.edu.sv>
2024-07-08 15:06:03 -0600

    Agregando presione enter para que el bot comience a decir numeros y agregando llaves en cada case

--- PRUEBAFINAL.cpp
+++ PRUEBAFINAL.cpp
@@ -34,7 +34,7 @@ int main(void)
 
         /*SWITCH PRINCIPAL*/
         switch (opc) {
-            case 1: 
+            case 1: {
                 /* CASE DE NUEVA PARTIDA */
                 cout << endl << endl << "NUEVA PARTIDA" << endl << endl;
 
@@ -77,35 +77,47 @@ int main(void)
 
                 }
 
+                /*PAUSA Y PRESIONAR ENTER PARA QUE EL BOT COMIENCE A GENERAR NUMEROS*/
+                cout << "Presione Enter para que el bot genere el primer numero..." << endl;
+                cin.ignore();
+                cin.get();
+
                 break;
+            }
             
-            case 2: 
+            case 2: {
                 cout << endl << "REGISTRO DE PARTIDAS" << endl << endl;
                 break; 
-            
-            case 3: 
+            }
+
+            case 3: {
                 cout << endl << "REGISTRO DE GANADORES" << endl << endl;
                 break;
+            }
             
-            case 4:
+            case 4: {
                 cout << endl << "INTEGRANTES DE MAX" << endl << endl;
                 cout << endl << "Ana Belen Ramirez Flores 00183224" << endl;
                 cout << endl << "Xochill Guissell Miguel Miranda 00114924" << endl;
                 cout << endl << "Maria Fernanda Ramirez Portillo 00127724" << endl;
                 cout << endl << endl;
                 break;
-            
-            case 5: 
+            }
+
+            case 5: {
                 cout << endl << "INSTRUCCIONES DEL JUEGO" << endl << endl;
                 break;
+            }
 
-            case 6:
+            case 6: {
                 cout << "SALIR " << endl;
                 break;
+            }
 
-            default: 
+            default: {
             
                 break;
+            }
         }
     } while (opc != 6);
 
